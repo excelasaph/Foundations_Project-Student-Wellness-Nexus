e16fc8ff8932d4717fca7915af31f6fb81780728
excel-asaph <excelasaph@gmail.com>
2024-04-17 20:38:24 +0200

    created forms.py

--- hospital/forms.py
+++ hospital/forms.py
@@ -63,10 +63,6 @@ class PatientAppointmentForm(forms.ModelForm):
         fields=['description','status']
 
 
-#for contact us page
-class ContactusForm(forms.Form):
-    Name = forms.CharField(max_length=30)
-    Email = forms.EmailField()
-    Message = forms.CharField(max_length=500,widget=forms.Textarea(attrs={'rows': 3, 'cols': 30}))
+
 
 
